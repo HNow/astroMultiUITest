885f39f05b928928c15cc7c7b9325093b7b553a2
HNow <hishamnasir.0.1@gmail.com>
2023-04-18 23:59:31 -0400

    initial commit

---
+++ src/components/ReactSample.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./ReactSampleStyles.css";
+
+type MyProps={
+
+
+};
+
+type MyState ={
+
+
+};
+
+
+class App extends React.Component<MyProps,MyState>{
+    state: MyState={
+
+    };
+
+render()
+{
+
+return(
+<div className="container">
+<div className="left">
+    <h1 className="cardTitle">Title! </h1>
+</div>
+<div className="right">
+
+
+</div>
+
+</div>
+
+);
+
+}
+
+}
+
+export default App;
+
+
